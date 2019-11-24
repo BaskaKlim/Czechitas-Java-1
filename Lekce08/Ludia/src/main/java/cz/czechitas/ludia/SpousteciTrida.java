265733c90d9786eb78cd6bce97011ff3d1eeae76
Baska Klimek <baska.klimekova@gmail.com>
2019-11-24 23:32:04 +0100

    zoznam mien do ktoreho pridavam z konzoly mena a vypisuje mi ich v opacnom poradi

---
+++ Lekce08/Ludia/src/main/java/cz/czechitas/ludia/SpousteciTrida.java
@@ -0,0 +1,26 @@
+package cz.czechitas.ludia;
+
+import java.util.*;
+import com.sun.org.apache.bcel.internal.generic.*;
+
+public class SpousteciTrida {
+
+    public static void main(String[] args) {
+
+        List<String> zoznamMien = new ArrayList<>();
+
+        Scanner nacitacRiadkov = new Scanner(System.in);
+        while (true) {
+            String meno = nacitacRiadkov.nextLine();
+            if (meno.isEmpty()) break;
+            // TODO: pridaj meno do zoznamu mien
+            zoznamMien.add(meno);
+        }
+        // TODO: vypis mena zo zoznamu mien
+        // idem cez indexy od posledny index, ktory je o -1 mensi ako velkost zoznamu  az po 0
+        for (int i = zoznamMien.size()- 1; i>=0; i--) {
+            System.out.println(zoznamMien.get(i));
+        }
+    }
+
+}
